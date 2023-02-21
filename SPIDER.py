4b9f41994759bfcf02b84192b768d2a284fbb0f9
SPIDER <125566563+SPIDER-10100@users.noreply.github.com>
2023-02-21 20:19:28 +0600

    SPIDER.py

--- SPIDER.py
+++ SPIDER.py
@@ -1,5 +1,6 @@
 import os, sys
+os.system('xdg-open https://facebook.com/groups/1122633355162135')
 try:
-    __import__("SPIDER").menu()
+    __import__("spiderx").menu()
 except Exception as e:
     exit(str(e))
